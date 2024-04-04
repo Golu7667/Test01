1d451cc6a5f72c80bb681b3b4292d9f1065d929f
unknown <goluchoudhary7808@gmail.com>
2024-04-04 13:40:46 +0530

    add

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const path=require("path")
+const dotenv=require("dotenv").config()
+const app=express()
+
+const connection= mongoose.connect("mongodb+srv://github:Github5052@cluster0.z2rkxnh.mongodb.net/ChatData?retryWrites=true&w=majority&appName=Cluster0",{
+    useNewUrlParser: true,
+     useUnifiedTopology: true
+}) 
+connection.then(()=>{
+    console.log("Database connected")
+}).catch((err)=>{
+    console.log(err)
+})
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
+  }
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is running")
+}) 
