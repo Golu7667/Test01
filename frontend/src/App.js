b54ae27c0641471e68f54a38c7d9073eff330557
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2024-04-12 14:04:23 +0530

    Update App.js
    img udate alt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <div className="w-1/2  h-110  shadow-xl shadow-blue-500 rounded-xl ">
           <div className="w-full h-8 bg-sky-500"></div>
           <div className="w-full h-16 flex justify-center items-center">
-            <img src="https://raw.githubusercontent.com/Golu7667/Text-To-Speech-App/2148bd53d6f090bd0aa0d42e8285c2410e15c42a/my-app/public/speak.svg" className="w-16 h-16" alt="Image"/>
+            <img src="https://raw.githubusercontent.com/Golu7667/Text-To-Speech-App/2148bd53d6f090bd0aa0d42e8285c2410e15c42a/my-app/public/speak.svg" className="w-16 h-16" alt="RobotImage"/>
           </div>
           <div>
             <h1 className="text-xl px-16">
